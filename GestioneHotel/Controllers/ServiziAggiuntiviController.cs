79efacc082d554be41d0f533bee7f9abe0803af2
GiampyLux <giampaolopaladino01@gmail.com>
2024-07-25 11:19:27 +0200

    commit di prova 2

--- GestioneHotel/Controllers/ServiziAggiuntiviController.cs
+++ GestioneHotel/Controllers/ServiziAggiuntiviController.cs
@@ -2,7 +2,6 @@
 using GestioneHotel.Models;
 using GestioneHotel.Services;
 using System.Collections.Generic;
-using System.Data.SqlClient;
 using System.Threading.Tasks;
 
 namespace GestioneHotel.Controllers
